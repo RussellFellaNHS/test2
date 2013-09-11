9d20bc3cd36fbdb6cb7ebda569adc98411b44e8c
RussellFellaNHS <chris@chris-VirtualBox.(none)>
2013-09-11 16:01:31 +0100

    dd

--- content_service.rb
+++ content_service.rb
@@ -1,11 +1,22 @@
 require 'sinatra'
 require './content_resolver'
 
+set :server, 'webrick'
+
+
 get '/content' do
+
+  @content_resolver = ContentResolver.new()
 
   #get content from service
 
-  cr = ContentResolver.new
-  cr.get_content_assets_by_uri
+  # # cr = ContentResolver.new
+  #cr.get_content_assets_by_uri
+
+  #@content_resolver.get_content_assets_by_uri("article/asthma/overview")
+
+  #puts @content_resolver.inspect
+
+  "a"
 
 end
